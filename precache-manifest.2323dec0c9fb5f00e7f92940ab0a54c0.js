0a534dfc8d00fe7a88d8d0462693c37e5a2b29cf
Vashar <vishal.pilani@live.com>
2019-06-27 13:33:27 +0530

    uploading build

---
+++ precache-manifest.2323dec0c9fb5f00e7f92940ab0a54c0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8a41d0479efc8e065325ffaa4d6aa61d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d1ebd76a6ed8790ec0b5",
+    "url": "/static/css/2.66b7f74f.chunk.css"
+  },
+  {
+    "revision": "c117fdf318a4b5cbc6d7",
+    "url": "/static/css/main.65edcfb4.chunk.css"
+  },
+  {
+    "revision": "d1ebd76a6ed8790ec0b5",
+    "url": "/static/js/2.d533a9b1.chunk.js"
+  },
+  {
+    "revision": "c117fdf318a4b5cbc6d7",
+    "url": "/static/js/main.4867414d.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
